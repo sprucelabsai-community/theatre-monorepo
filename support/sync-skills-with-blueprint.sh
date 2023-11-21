3e1983e8ce8499e0ad5bfc45bb5633361906ec43
Taylor Romero <me@taylorrome.ro>
2023-11-21 13:28:50 -0700

    patch: log test

--- support/sync-skills-with-blueprint.sh
+++ support/sync-skills-with-blueprint.sh
@@ -39,7 +39,9 @@ PIDS=()
 # Loop over each repo and attempt to add in the background
 for REPO in $REPOS; do
     # Run add-skill.sh in the background
-    git clone $REPO
+
+    echo "git clone $REPO"
+
     # ./add-skill.sh $REPO $1
     # Store the PID of the background process
     PIDS+=($!)
