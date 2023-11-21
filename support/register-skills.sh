2d81e91c5282cd48a8d5ddc336bae29878da9638
Taylor Romero <me@taylorrome.ro>
2023-11-21 09:14:00 -0700

    minor: skill registeration progress

--- support/register-skills.sh
+++ support/register-skills.sh
@@ -6,14 +6,26 @@ for dir in *-skill; do
     if [[ -d $dir ]]; then
         cd "$dir"
 
+        # Extract NAMESPACE from directory name
+        IFS='-' read -ra ADDR <<<"$dir"
+        if [ ${#ADDR[@]} -gt 1 ]; then
+            NAMESPACE=${ADDR[1]}
+        else
+            echo "Invalid directory format for $dir" >&2
+            cd ..
+            continue
+        fi
+
         # Check if .env file exists
         if [ -f .env ]; then
             # Check if SKILL_ID is defined in .env file
             if ! grep -q "^SKILL_ID=" .env; then
-                echo "$dir NEEDS REGISTER"
+                spruce set.remote --remote=local
+                spruce register --nameReadable="$NAMESPACE" --nameKebab="$NAMESPACE"
             fi
         else
-            echo "$dir NEEDS REGISTER (No .env file found)"
+            echo "$dir is missing a .env file!" >&2
+            exit 1
         fi
 
         cd ..
