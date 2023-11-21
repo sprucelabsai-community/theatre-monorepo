0405d4330a4e57df4ee2e66befa5d6550cad31ca
Taylor Romero <me@taylorrome.ro>
2023-11-21 13:28:25 -0700

    patch: test

--- support/sync-skills-with-blueprint.sh
+++ support/sync-skills-with-blueprint.sh
@@ -39,7 +39,7 @@ PIDS=()
 # Loop over each repo and attempt to add in the background
 for REPO in $REPOS; do
     # Run add-skill.sh in the background
-    git clone git@github.com:sprucelabsai/spruce-mercury-api.git
+    git clone $REPO
     # ./add-skill.sh $REPO $1
     # Store the PID of the background process
     PIDS+=($!)
