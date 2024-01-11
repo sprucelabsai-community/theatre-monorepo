db4651d3673cd88251290e39a9d31012c96ed284
Taylor Romero <me@taylorrome.ro>
2024-01-10 19:38:28 -0700

    patch: test

--- support/reset-core-db.sh
+++ support/reset-core-db.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 # Generate a random 5-digit PIN
-PIN=$(shuf -i 10000-99999 -n 1)
+PIN=$(((RANDOM % 90000) + 10000))
 echo "You are about to destroy the core database, enter this pin to confirm: $PIN"
 
 # Ask the user to enter the PIN
-PIN=$(((RANDOM % 90000) + 10000))
-echo "Generated PIN: $PIN"
+read -p "Enter the PIN: " entered_pin
 
 # Compare the entered PIN with the generated PIN
 if [ "$entered_pin" == "$PIN" ]; then
@@ -14,5 +13,4 @@ if [ "$entered_pin" == "$PIN" ]; then
     mongosh --quiet --eval 'db.getMongo().getDBNames().forEach(function(i){try { console.log("Dropping",i);db.getSiblingDB(i).dropDatabase() } catch {}})'
 else
     echo "Incorrect PIN. Aborting operation."
-    exit 1
 fi
