cea415811f62e8211ee824e4e95942ab304ed2f4
Taylor Romero <me@taylorrome.ro>
2024-01-14 16:24:52 -0700

    patch: build heartwood test

--- support/build-heartwood.sh
+++ support/build-heartwood.sh
@@ -3,5 +3,8 @@
 cd packages/spruce-heartwood-skill
 yarn build.cdn
 
+# Get the current timestamp
 timestamp=$(date +%s)
-sed -i "s/{{version}}/${timestamp}/g" ./dist/index.html
+
+# Replace {{version}} with the current timestamp in ./dist/index.html
+sed -i '' "s/{{version}}/${timestamp}/g" ./dist/index.html
