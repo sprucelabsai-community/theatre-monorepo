4986ca2a7b57fdc0420db8e0df761fdaa1c0c28f
Taylor Romero <me@taylorrome.ro>
2024-01-14 18:13:35 -0700

    patch: run

--- support/serve-heartwood.sh
+++ support/serve-heartwood.sh
@@ -6,9 +6,6 @@ DIR="$(pwd)"
 # Define the path to the heartwood-skill directory
 heartwood_skill_dir="$DIR/theatre-monorepo/packages/spruce-heartwood-skill"
 
-# Define the directory to serve
-SERVE_DIR="$heartwood_skill_dir/dist"
-
 # Create a Caddyfile
 echo "http://0.0.0.0:8080
 root * $SERVE_DIR
@@ -16,7 +13,7 @@ file_server" >Caddyfile
 
 # Check if the heartwood-skill directory exists
 if [ ! -d "$heartwood_skill_dir" ]; then
-    echo "Error: The /heartwood-skill directory does not exist. You need to run 'npm run sync [pathToBlueprint.yml]'"
+    echo "Error: The $heartwood_skill_dir directory does not exist. You need to run 'npm run sync [pathToBlueprint.yml]'"
     exit 1
 fi
 
