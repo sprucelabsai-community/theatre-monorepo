d2afc153a30cd20ff876f38d13a9b469c2af9f10
Taylor Romero <me@taylorrome.ro>
2024-01-18 12:45:47 -0700

    major: reset core database change

--- support/reset-core-db.sh
+++ support/reset-core-db.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# Initialize dump_file variable
-dump_file=""
+# Initialize dump_dir variable
+dump_dir=""
+should_use_pin=true
 
 # Function to show usage
 usage() {
-    echo "Usage: yarn reset.core.database --dumpDir=/path/to/dump"
+    echo "Usage: yarn reset.core.database --dumpDir=/path/to/dump [--shouldUsePin=false]"
     exit 1
 }
 
@@ -13,7 +14,11 @@ usage() {
 for arg in "$@"; do
     case $arg in
     --dumpDir=*)
-        dump_file="${arg#*=}"
+        dump_dir="${arg#*=}"
+        shift # Remove argument from processing
+        ;;
+    --shouldUsePin=false)
+        should_use_pin=false
         shift # Remove argument from processing
         ;;
     *)
@@ -23,52 +28,54 @@ for arg in "$@"; do
     esac
 done
 
-# Generate a random 5-digit PIN
-PIN=$(((RANDOM % 90000) + 10000))
-echo "You are about to reset the core database. Enter this pin to confirm: $PIN"
+# Generate a random 5-digit PIN if should_use_pin is true
+if [ "$should_use_pin" = false ]; then
+    echo "PIN check bypassed."
+else
+    PIN=$(((RANDOM % 90000) + 10000))
+    echo "You are about to reset the core database. Enter this pin to confirm: $PIN"
 
-# Ask the user to enter the PIN
-read -p "Enter the PIN: " entered_pin
+    # Ask the user to enter the PIN
+    read -p "Enter the PIN: " entered_pin
 
-# Compare the entered PIN with the generated PIN
-if [ "$entered_pin" == "$PIN" ]; then
-    echo "PIN verified."
-
-    # Ask for the location of dump file if not provided as an argument
-    if [ -z "$dump_file" ]; then
-        read -p "Enter the location of dump you want to restore from (empty to skip): " dump_file
-    fi
-
-    # if the dump_file is provided, but no dir exists, throw an error
-    if [ -n "$dump_file" ] && [ ! -d "$dump_file" ]; then
-        echo "Dump file not founda at $dump_file. Aborting operation."
+    # Compare the entered PIN with the generated PIN
+    if [ "$entered_pin" != "$PIN" ]; then
+        echo "Incorrect PIN. Aborting operation."
         exit 1
     fi
-
-    # Clear the core database
-    echo "Clearing core database..."
-    mongosh --quiet --eval 'db.getMongo().getDBNames().forEach(function(i){try { console.log("Dropping",i);db.getSiblingDB(i).dropDatabase() } catch {}})'
-
-    # Restore from dump if the dump_file is provided and valid
-    if [ -n "$dump_file" ]; then
-        echo "Dump file found. Restoring databases now..."
-        mongorestore --dir "$dump_file"
-        echo "Restore complete!"
-    else
-        echo "Database cleared!"
-    fi
-
-    echo "Next steps:"
-    echo "1. Shutdown the platform: yarn shutdown"
-    echo "2. Start mercury: yarn boot.mercury"
-    echo "3. Login: yarn login blueprint.yml"
-    if [ -n "$dump_file" ]; then
-        echo "4. Register skills: yarn register"
-    else
-        echo "4. Register skills: yarn register -shouldForceRegister=true"
-    fi
-    echo "5. Reboot the platform: yarn reboot"
-
-else
-    echo "Incorrect PIN. Aborting operation."
 fi
+
+# Ask for the location of dump directory if not provided as an argument
+if [ -z "$dump_dir" ]; then
+    read -p "Enter the location of dump you want to restore from (empty to skip): " dump_dir
+fi
+
+# If the dump_dir is provided, but no directory exists, throw an error
+if [ -n "$dump_dir" ] && [ ! -d "$dump_dir" ]; then
+    echo "Dump directory not found at $dump_dir. Aborting operation."
+    exit 1
+fi
+
+# Clear the core database
+echo "Clearing core database..."
+mongosh --quiet --eval 'db.getMongo().getDBNames().forEach(function(i){try { console.log("Dropping",i);db.getSiblingDB(i).dropDatabase() } catch {}})'
+
+# Restore from dump if the dump_dir is provided and valid
+if [ -n "$dump_dir" ]; then
+    echo "Dump directory found. Restoring databases now..."
+    mongorestore --dir "$dump_dir"
+    echo "Restore complete!"
+else
+    echo "Database cleared!"
+fi
+
+echo "Next steps:"
+echo "1. Shutdown the platform: yarn shutdown"
+echo "2. Start mercury: yarn boot.mercury"
+echo "3. Login: yarn login blueprint.yml"
+if [ -n "$dump_dir" ]; then
+    echo "4. Register skills: yarn register"
+else
+    echo "4. Register skills: yarn register -shouldForceRegister=true"
+fi
+echo "5. Reboot the platform: yarn reboot"
