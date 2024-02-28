2cc438e2875a91eb1ed9161b8c89959aec11a0a0
Taylor Romero <me@taylorrome.ro>
2024-02-28 06:13:38 -0700

    minor: can upgrade individual skill (upgrade.skill)

--- support/test-clone.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git clone -v git@github.com:sprucelabsai/spruce-mercury-api.git
---
+++ support/upgrade-skill.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Default vendor
+vendor="spruce"
+
+# Check for at least one argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <namespace> [vendor]"
+    echo "Example: $0 heartwood"
+    exit 1
+fi
+
+# Assign arguments
+namespace="$1"
+if [ $# -ge 2 ]; then
+    vendor="$2"
+fi
+
+# Set directory name based on namespace
+if [ "$namespace" == "mercury" ]; then
+    skill_dir_name="${vendor}-${namespace}-api"
+else
+    skill_dir_name="${vendor}-${namespace}-skill"
+fi
+
+cd packages/$skill_dir_name
+
+git checkout .
+git pull
+rm yarn.lock
+rm package-lock.json
+spruce upgrade
