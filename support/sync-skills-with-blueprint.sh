6b12187b76c865912bd6883c2232db84acd5c0bf
alexmakijokela <alex.makijokela@gmail.com>
2024-04-06 16:23:05 -0600

    minor: prompt and rm unused blueprint skills on sync

--- support/sync-skills-with-blueprint.sh
+++ support/sync-skills-with-blueprint.sh
@@ -40,6 +40,30 @@ PIDS=()
 # We skip the first argument since it's the path to the blueprint.yml
 ADDITIONAL_ARGS="${@:2}"
 
+PM2_SKILLS=$(pm2 list | grep -- '-skill' | awk '{print $4}')
+# Prompt the user for each skill to remove
+for SKILL in $PM2_SKILLS; do
+  echo $SKILL
+  SKILL_FOUND=false
+  for REPO in $REPOS; do
+  echo $REPO
+    REPO_NAME=$(basename "$REPO" .git\")
+    echo $REPO_NAME
+    if [[ "$REPO_NAME" == "$SKILL" ]]; then
+      SKILL_FOUND=true
+      break
+    fi
+  done
+  
+  if [ "$SKILL_FOUND" = false ]; then
+    read -p "Do you want to remove the skill '$SKILL' from the PM2 setup? (y/n): " REMOVE_SKILL
+    if [[ $REMOVE_SKILL =~ ^[Yy]$ ]]; then
+      # Remove the skill from the PM2 setup
+      pm2 delete $SKILL
+    fi
+  fi
+done
+
 # Loop over each repo and attempt to add in the background
 for REPO in $REPOS; do
 
