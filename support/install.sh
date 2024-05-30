dbb93e7f6b831bdec3eb047efc7385c0c18f87a4
Taylor Romero <me@taylorrome.ro>
2024-05-30 15:08:17 -0600

    patch: unmount to specific dir

--- support/install.sh
+++ support/install.sh
@@ -246,10 +246,17 @@ read -r blueprint_path
 if [ -z "$blueprint_path" ]; then
     echo "Downloading Sprucebot Development Theatre..."
     curl -o ~/Downloads/Sprucebot+Theatre-arm64.dmg https://s3.amazonaws.com/developer.spruce.bot/development-theatre/Sprucebot+Theatre-arm64.dmg
+
     echo "Installing Sprucebot Development Theatre..."
-    hdiutil attach ~/Downloads/Sprucebot+Theatre-arm64.dmg
+    # Mount the DMG file
+    hdiutil attach ~/Downloads/Sprucebot+Theatre-arm64.dmg -mountpoint /Volumes/Sprucebot\ Theatre
+
+    # Copy the app to the Applications folder
     cp -R /Volumes/Sprucebot\ Theatre/Sprucebot\ Theatre.app /Applications
+
+    # Unmount the DMG file
     hdiutil detach /Volumes/Sprucebot\ Theatre
+
     echo "Sprucebot Development Theatre installed into Applications..."
     open /Applications/Sprucebot\ Theatre.app
 
