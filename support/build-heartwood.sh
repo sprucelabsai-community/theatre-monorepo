b268c89ab045968f4c879df11713657bfd91ea38
alexmakijokela <alex.makijokela@gmail.com>
2024-07-25 16:22:31 -0600

    patch: cross-platform sed

--- support/build-heartwood.sh
+++ support/build-heartwood.sh
@@ -13,5 +13,11 @@ yarn build.cdn
 # Get the current timestamp
 timestamp=$(date +%s)
 
-# Replace {{version}} with the current timestamp in ./dist/index.html
-sed -i '' "s/{{version}}/${timestamp}/g" ./dist/index.html
+# Cross-platform sed in-place edit
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  sed -i '' "s/{{version}}/${timestamp}/g" ./dist/index.html
+else
+  # Linux and others
+  sed -i "s/{{version}}/${timestamp}/g" ./dist/index.html
+fi
