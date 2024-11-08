0034c594695846adb66d67a03d9b882c4c51d1ba
Taylor Romero <me@taylorrome.ro>
2024-11-08 12:47:14 -0700

    patch: sync events fix

--- support/sync-events.sh
+++ support/sync-events.sh
@@ -5,8 +5,12 @@ source ./support/hero.sh
 for dir in packages/*-skill; do
     (
         cd $dir
-        spruce sync.events
+        spruce sync.events >>/dev/null
     ) &
 done
 
+hero "Syncing events, this may take a few minutes..."
+
 wait
+
+yarn build
