02a52753b012597fa4749faf8a9269a406edd81c
Taylor Romero <me@taylorrome.ro>
2025-01-20 14:48:19 -0700

    patch: oops

--- support/build.sh
+++ support/build.sh
@@ -16,7 +16,7 @@ if [ "$BUILD_STRATEGY" != null ]; then
     build_strategy=$BUILD_STRATEGY
 fi
 
-if [ "$build_strategy" = "series" ]; then
+if [ "$build_strategy" = "serial" ]; then
     echo "Building skills in series..."
     yarn run build.serial
 else
