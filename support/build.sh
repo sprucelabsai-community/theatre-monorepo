bb23a21dbe1cddd258e84a48838fcc6f504c169a
Taylor Romero <me@taylorrome.ro>
2025-01-20 12:56:36 -0700

    minor: new BUILD_STRATEGY in blueprint.yml

---
+++ support/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source ./support/hero.sh
+
+hero "Building skills..."
+
+echo "Checking build strategy..."
+
+build_strategy="parallel"
+should_boot_message_receiver=false
+
+THEATRE=$(node ./support/blueprint.js blueprint.yml theatre)
+BUILD_STRATEGY=$(echo "$THEATRE" | jq -r '.BUILD_STRATEGY' 2>/dev/null)
+
+if [ "$BUILD_STRATEGY" != null ]; then
+    build_strategy=$BUILD_STRATEGY
+fi
+
+if [ "$build_strategy" = "parallel" ]; then
+    echo "Building skills in parallel..."
+    yarn run build.parallel
+else
+    echo "Building skills in series..."
+    yarn run build.serial
+fi
