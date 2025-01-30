310aa6735266e0e0d870ed0b4bfb6acbf96f517a
Taylor Romero <me@taylorrome.ro>
2025-01-30 07:51:38 -0700

    patch: mongo fix

--- support/install.sh
+++ support/install.sh
@@ -23,7 +23,7 @@ echo "
                                                                          
 "
 
-echo "Version: 3.5.12"
+echo "Version: 3.5.13"
 
 setupTheatreUntil=""
 setupMode=""
@@ -260,7 +260,7 @@ install_mongo() {
         curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
         echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
         sudo apt-get update
-        sudo apt-get install -y mongodb-org-database mongodb-org
+        sudo apt-get install -y mongodb-org-server mongodb-org-mongos mongodb-org-database mongodb-org
     else
         echo "Unsupported package manager. Please install MongoDB manually."
         exit 1
