4a92203542a26e18cfbb57b47c903e385b1a6ef3
Taylor Romero <me@taylorrome.ro>
2025-05-21 14:54:10 -0600

    major: POST_BUNDLE_SCRIPT moved to theatre level

--- support/bundle-heartwood.sh
+++ support/bundle-heartwood.sh
@@ -5,7 +5,7 @@ blueprint="blueprint.yml"
 
 ENV=$(node support/blueprint.js $blueprint env)
 PUBLIC_ASSETS_DIR=$(echo "$ENV" | jq -r '.heartwood[] | select(has("PUBLIC_ASSETS_DIR")) | .PUBLIC_ASSETS_DIR' 2>/dev/null)
-POST_BUNDLE_SCRIPT=$(echo "$ENV" | jq -r '.heartwood[] | select(has("POST_BUNDLE_SCRIPT")) | .POST_BUNDLE_SCRIPT' 2>/dev/null)
+POST_BUNDLE_SCRIPT=$(echo "$ENV" | jq -r '.theatre[] | select(has("POST_BUNDLE_SCRIPT")) | .POST_BUNDLE_SCRIPT' 2>/dev/null)
 
 heartwood_dir="packages/spruce-heartwood-skill"
 
