f50ef10c104202ab49cc7ce044c10e0171e221c7
Taylor Romero <me@taylorrome.ro>
2025-10-02 00:22:38 -0600

    patch: propagate fixes

--- support/propagate-blueprint.sh
+++ support/propagate-blueprint.sh
@@ -91,21 +91,71 @@ for i in "${!keys[@]}"; do
 	fi
 done
 
-# Deduplicate .env entries, preserving comments and blank lines, keeping only the last value for each key
+# Deduplicate .env entries while retaining multi-line values and comments.
+# Each assignment block (key line plus any continuation lines until the next key/comment/blank) is buffered
+# so we can keep only the last occurrence for each key without mangling private keys or other multi-line secrets.
 awk '
-    /^[[:space:]]*#/ || /^[[:space:]]*$/ { print; next }
-    /^[A-Za-z_][A-Za-z0-9_]*=/ {
-        key = $1
-        sub(/=.*/, "", key)
-        last[key] = NR
-        line[NR] = $0
-        key_for_line[NR] = key
+    function flush_comment_block() {
+        if (comment_buf != "") {
+            printf "%s", comment_buf
+            comment_buf = ""
+        }
+    }
+
+    function flush_assignment(key) {
+        if (key == "") {
+            return
+        }
+        last_block[key] = block_index
+        blocks[block_index] = block_buf
+        keys_for_block[block_index] = key
+        block_buf = ""
+    }
+
+    function flush_pending() {
+        flush_assignment(current_key)
+        current_key = ""
+    }
+
+    /^[[:space:]]*$/ {
+        flush_pending()
+        flush_comment_block()
+        print
         next
     }
-    { print }
+
+    /^[[:space:]]*#/ {
+        flush_pending()
+        comment_buf = comment_buf $0 "\n"
+        next
+    }
+
+    /^[A-Za-z_][A-Za-z0-9_]*=/ {
+        flush_pending()
+        flush_comment_block()
+        ++block_index
+        current_key = $1
+        sub(/=.*/, "", current_key)
+        block_buf = $0 "\n"
+        next
+    }
+
+    {
+        if (current_key != "") {
+            block_buf = block_buf $0 "\n"
+        } else {
+            printf "%s\n", $0
+        }
+    }
+
     END {
-        for (i=1; i<=NR; i++) {
-            if (line[i] != "" && last[key_for_line[i]] == i) print line[i]
+        flush_pending()
+        flush_comment_block()
+        for (i = 1; i <= block_index; ++i) {
+            key = keys_for_block[i]
+            if (last_block[key] == i) {
+                printf "%s", blocks[i]
+            }
         }
     }
 ' .env >.env.cleaned && mv .env.cleaned .env
